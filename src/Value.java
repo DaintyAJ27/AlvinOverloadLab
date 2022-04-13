7c8b3ffb6e92e3a84e957c10d2246152a7fa07b6
DaintyAJ27 <legendarygmer9@gmail.com>
2022-04-13 11:37:09 +0800

    commit

---
+++ src/Value.java
@@ -0,0 +1,12 @@
+public class Value {
+    //This code will get the greatest value in two integers
+    public static double greaterValue(double d, double e) {
+        double result;
+       if (d > e)     
+         result = (int) d;
+       else
+         result = (double) e;
+        return result;
+    }
+    
+}//end of class
